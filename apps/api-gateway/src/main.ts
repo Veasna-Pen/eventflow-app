34cdec57acd63252ad10cd39aa19c0461d0e6520
Veasna-Pen <veasna3d@gmail.com>
2026-07-22 16:11:47 +0700

    add: api-gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SERVICE_PORTS } from '@app/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(SERVICE_PORTS.API_GATEWAY);
 
   console.log(`API Gateway is running on port ${SERVICE_PORTS.API_GATEWAY}`);
